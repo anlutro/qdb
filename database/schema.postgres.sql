842b6932facac41b56931a5d29dab320354605d5
Andreas Lutro <anlutro@gmail.com>
2015-04-02 15:20:35 +0200

    guess this isn't needed

--- database/schema.postgres.sql
+++
@@ -1,6 +0,0 @@
-create table quotes (
-	id serial primary key,
-	body text,
-	submitted_at timestamp,
-	approved boolean default false
-);
--- database/schema.sqlite.sql
+++
@@ -1,6 +0,0 @@
-create table quotes (
-	id integer primary key,
-	body text,
-	submitted_at timestamp,
-	approved boolean default false
-);
