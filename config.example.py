793ba9f12814be9899963df9b1848a91ca530516
Andreas Lutro <anlutro@gmail.com>
2015-04-02 09:28:20 +0200

    comment config example

--- config.example.py
+++ config.example.py
@@ -1,4 +1,13 @@
+# enable or disable debug mode
 DEBUG = True
+
+# secret key used for session stuff
 SECRET_KEY = 'IAMTOTALLYNOTSECRET!'
+
+# the moderator password
 PASSWORD = 'whathefuck'
+
+# the sqlite database string
+# http://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls
 DB = 'sqlite:////home/myuser/qdb/tmp/test.db'
+# DB = 'postgresql://scott:qdb@localhost/qdb'
