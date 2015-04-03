e8bd8398a7fa0603c1a616e06e7dfe81547b11cf
Andreas Lutro <anlutro@gmail.com>
2015-04-03 12:26:13 +0200

    cleanup

--- qdb/__init__.py
+++ qdb/__init__.py
@@ -1,5 +1,4 @@
-from flask import (Flask, url_for, render_template, jsonify,
-	redirect, abort, flash, request, session)
+from flask import Flask, session
 app = Flask(__name__)
 
 # find the project root directory
@@ -10,17 +9,17 @@ dirname = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 cfg_file_path = os.path.join(dirname, 'config.py')
 app.config.from_pyfile(cfg_file_path)
 
-# enable bytecode caching for templates
-from jinja2 import FileSystemBytecodeCache
-from werkzeug.datastructures import ImmutableDict
-j2cachedir = os.path.join(dirname, 'tmp', 'j2cache')
-app.jinja_options = ImmutableDict(
-	extensions = ['jinja2.ext.with_'],
-	bytecode_cache = FileSystemBytecodeCache(j2cachedir, '%s.cache'),
-)
-
-# set up logging to stderr
 if not app.debug:
+	# enable bytecode caching for templates
+	from jinja2 import FileSystemBytecodeCache
+	from werkzeug.datastructures import ImmutableDict
+	j2cachedir = os.path.join(dirname, 'tmp', 'j2cache')
+	app.jinja_options = ImmutableDict(
+		extensions = ['jinja2.ext.with_'],
+		bytecode_cache = FileSystemBytecodeCache(j2cachedir, '%s.cache'),
+	)
+
+	# set up logging to stderr
 	import logging, sys
 	file_handler = logging.StreamHandler(sys.stderr)
 	file_handler.setLevel(logging.WARNING)
