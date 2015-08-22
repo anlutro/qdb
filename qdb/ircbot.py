e2d168a0f6e97fbbcd81214f1a089d06559b8be2
Andreas Lutro <anlutro@gmail.com>
2015-08-22 11:46:03 +0200

    update ircbot integration

--- qdb/ircbot.py
+++ qdb/ircbot.py
@@ -1,15 +1,12 @@
-from flask import jsonify
 import json
 import urllib.request
 
-BASE_URL = 'http://localhost:9123'
+from qdb import app
 
-def make_post_request(path, quote):
-	data = {'quote': quote.to_json_dict()}
-	body = json.dumps(data).encode('utf-8')
-	url = BASE_URL + path
-
-	urllib.request.urlopen(url, body, timeout=1)
 
 def notify_bot(quote):
-	make_post_request('/qdb-update', quote)
+	data = {'quote': quote.to_json_dict()}
+	body = json.dumps(data).encode('utf-8')
+	url = app.config.get('IRCBOT_WEBHOOK_URL')
+
+	urllib.request.urlopen(url, body, timeout=1)
