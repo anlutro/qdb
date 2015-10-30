ad243447680f57c268169164c19aebce8fcdc0e7
Simon de Vlieger <simon@qabana.nl>
2015-10-30 13:24:31 +0100

    Python 2.7 compatibility and PEP-8.
    Replace urllib.request with the requests module
    Python 2's math.ceil returns a float.
    urllib.parse was called urlparse
    
    Retab every file to spaces.
    Fix all hanging indents.
    Remove unused imports.
    Skip the sqlalchemy comparisons in filter for false on
    value instead of identity.

--- qdb/models.py
+++ qdb/models.py
@@ -3,48 +3,54 @@ from qdb.database import Base
 import re
 import datetime
 
+
 class Quote(Base):
-	__tablename__ = 'quotes'
+    __tablename__ = 'quotes'
 
-	id = Column(Integer, primary_key=True)
-	body = Column(Text)
-	submitted_at = Column(DateTime)
-	submitter_ip = Column(Text)
-	approved = Column(Boolean)
+    id = Column(Integer, primary_key=True)
+    body = Column(Text)
+    submitted_at = Column(DateTime)
+    submitter_ip = Column(Text)
+    approved = Column(Boolean)
 
-	def __init__(self, body, submitted_at, submitter_ip, approved=False):
-		self.body = body
-		self.submitted_at = submitted_at
-		self.submitter_ip = submitter_ip
-		self.approved = approved
+    def __init__(self, body, submitted_at, submitter_ip, approved=False):
+        self.body = body
+        self.submitted_at = submitted_at
+        self.submitter_ip = submitter_ip
+        self.approved = approved
 
-	@property
-	def submitted_at_print(self):
-		dt = self.submitted_at
-		if dt.hour == 0 and dt.minute == 0 and dt.second == 0 and dt.microsecond == 0:
-			return datetime.date(dt.year, dt.month, dt.day)
-		return dt
+    @property
+    def submitted_at_print(self):
+        dt = self.submitted_at
+        if (dt.hour == 0 and
+           dt.minute == 0 and
+           dt.second == 0 and
+           dt.microsecond == 0):
+            return datetime.date(dt.year, dt.month, dt.day)
+        return dt
 
-	@staticmethod
-	def prepare(quote):
-		# remove timestamps from the start of each line in the quote
-		expr = re.compile(r'^[\[\(]?\d{1,2}\:?\d{2}(\:?\d{2})?(\s*(AM|PM))?[\]\)]?\s*')
-		lines = []
-		for line in quote.split('\n'):
-			lines.append(expr.sub('', line))
-		quote = '\n'.join(lines)
+    @staticmethod
+    def prepare(quote):
+        # remove timestamps from the start of each line in the quote
+        expr = re.compile(
+            r'^[\[\(]?\d{1,2}\:?\d{2}(\:?\d{2})?(\s*(AM|PM))?[\]\)]?\s*'
+        )
+        lines = []
+        for line in quote.split('\n'):
+            lines.append(expr.sub('', line))
+        quote = '\n'.join(lines)
 
-		# replace tabs with spaces
-		quote = re.sub(r'\t+', ' ', quote)
-		# remove windows-style newline characters
-		quote = quote.replace('\r', '')
+        # replace tabs with spaces
+        quote = re.sub(r'\t+', ' ', quote)
+        # remove windows-style newline characters
+        quote = quote.replace('\r', '')
 
-		return quote
+        return quote
 
-	def to_json_dict(self):
-		return {
-			'id': self.id,
-			'body': self.body,
-			'submitted_at': self.submitted_at.isoformat(),
-			'approved': self.approved,
-		}
+    def to_json_dict(self):
+        return {
+            'id': self.id,
+            'body': self.body,
+            'submitted_at': self.submitted_at.isoformat(),
+            'approved': self.approved,
+        }
--- qdb/routes.py
+++ qdb/routes.py
@@ -1,8 +1,16 @@
 from qdb import app
 
 from datetime import datetime
-from flask import (url_for, render_template, jsonify,
-	redirect, abort, flash, request, session)
+from flask import (
+    url_for,
+    render_template,
+    jsonify,
+    redirect,
+    abort,
+    flash,
+    request,
+    session
+)
 from qdb.models import Quote
 from qdb.database import db_session
 from qdb.utils import Paginator
@@ -12,176 +20,184 @@ import qdb.ircbot as ircbot
 
 @app.route('/')
 def home():
-	query = db_session.query(Quote) \
-		.filter(Quote.approved == True)
+    query = db_session.query(Quote) \
+        .filter(Quote.approved == True)
 
-	order = request.args.get('order', 'desc').lower()
-	if order == 'asc':
-		query = query.order_by(Quote.submitted_at.asc()) \
-			.order_by(Quote.id.asc())
-	else:
-		query = query.order_by(Quote.submitted_at.desc()) \
-			.order_by(Quote.id.desc())
+    order = request.args.get('order', 'desc').lower()
+    if order == 'asc':
+        query = query.order_by(Quote.submitted_at.asc()) \
+            .order_by(Quote.id.asc())
+    else:
+        query = query.order_by(Quote.submitted_at.desc()) \
+            .order_by(Quote.id.desc())
 
-	search = request.args.get('s')
-	if search:
-		query = query.filter(Quote.body.ilike('%'+search+'%'))
+    search = request.args.get('s')
+    if search:
+        query = query.filter(Quote.body.ilike('%'+search+'%'))
 
-	page = int(request.args.get('p', 1))
-	if page < 1:
-		page = 1
-	paginator = Paginator(query, page, request.url)
-	quotes = paginator.items
+    page = int(request.args.get('p', 1))
+    if page < 1:
+        page = 1
+    paginator = Paginator(query, page, request.url)
+    quotes = paginator.items
 
-	if request.headers.get('Accept') == 'application/json':
-		return jsonify(quotes=paginator)
+    if request.headers.get('Accept') == 'application/json':
+        return jsonify(quotes=paginator)
 
-	return render_template('list.html.jinja', quotes=quotes, paginator=paginator)
+    return render_template(
+        'list.html.jinja',
+        quotes=quotes,
+        paginator=paginator
+    )
 
 
 @app.route('/all')
 def get_all():
-	query = db_session.query(Quote) \
-		.filter(Quote.approved == True)
+    query = db_session.query(Quote) \
+        .filter(Quote.approved == True)
 
-	order = request.args.get('order', 'desc').lower()
-	if order == 'asc':
-		query = query.order_by(Quote.submitted_at.asc()) \
-			.order_by(Quote.id.asc())
-	else:
-		query = query.order_by(Quote.submitted_at.desc()) \
-			.order_by(Quote.id.desc())
+    order = request.args.get('order', 'desc').lower()
+    if order == 'asc':
+        query = query.order_by(Quote.submitted_at.asc()) \
+            .order_by(Quote.id.asc())
+    else:
+        query = query.order_by(Quote.submitted_at.desc()) \
+            .order_by(Quote.id.desc())
 
-	quotes = query.all()
+    quotes = query.all()
 
-	if request.headers.get('Accept') == 'application/json':
-		return jsonify(quotes=quotes)
+    if request.headers.get('Accept') == 'application/json':
+        return jsonify(quotes=quotes)
 
-	return render_template('list.html.jinja', quotes=quotes)
+    return render_template('list.html.jinja', quotes=quotes)
 
 
 @app.route('/random')
 def random():
-	query = db_session.query(Quote)
+    query = db_session.query(Quote)
 
-	search = request.args.get('s')
-	if search:
-		query = query.filter(Quote.body.ilike('%'+search+'%'))
+    search = request.args.get('s')
+    if search:
+        query = query.filter(Quote.body.ilike('%'+search+'%'))
 
-	query = query.filter(Quote.approved == True) \
-		.order_by(func.random()) \
-		.limit(10)
+    query = query.filter(Quote.approved == True) \
+        .order_by(func.random()) \
+        .limit(10)
 
-	quotes = query.all()
+    quotes = query.all()
 
-	if request.headers.get('Accept') == 'application/json':
-		return jsonify(quotes=quotes)
+    if request.headers.get('Accept') == 'application/json':
+        return jsonify(quotes=quotes)
 
-	return render_template('list.html.jinja', quotes=quotes)
+    return render_template('list.html.jinja', quotes=quotes)
 
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
-	if request.method == 'GET' or request.method == 'HEAD':
-		return render_template('form.html.jinja',
-			form_action=url_for('submit'),
-			body='',
-		)
+    if request.method == 'GET' or request.method == 'HEAD':
+        return render_template(
+            'form.html.jinja',
+            form_action=url_for('submit'),
+            body='',
+        )
 
-	body = Quote.prepare(request.form['body'])
-	quote = Quote(body, datetime.now(), request.remote_addr)
-	db_session.add(quote)
-	db_session.commit()
+    body = Quote.prepare(request.form['body'])
+    quote = Quote(body, datetime.now(), request.remote_addr)
+    db_session.add(quote)
+    db_session.commit()
 
-	if app.config.get('ENABLE_IRCBOT_WEBHOOKS'):
-		ircbot.notify_bot(quote)
+    if app.config.get('ENABLE_IRCBOT_WEBHOOKS'):
+        ircbot.notify_bot(quote)
 
-	flash('Quote added!')
-	return redirect(url_for('home'))
+    flash('Quote added!')
+    return redirect(url_for('home'))
 
 
 @app.route('/pending')
 def pending():
-	if not session.get('logged_in'):
-		flash('You must be logged in to do that!')
-		return redirect(url_for('home'))
+    if not session.get('logged_in'):
+        flash('You must be logged in to do that!')
+        return redirect(url_for('home'))
 
-	query = db_session.query(Quote) \
-		.filter(Quote.approved == False) \
-		.order_by(Quote.submitted_at.desc())
+    query = db_session.query(Quote) \
+        .filter(Quote.approved == False) \
+        .order_by(Quote.submitted_at.desc())
 
-	quotes = query.all()
+    quotes = query.all()
 
-	if request.headers.get('Accept') == 'application/json':
-		return jsonify(quotes=quotes)
+    if request.headers.get('Accept') == 'application/json':
+        return jsonify(quotes=quotes)
 
-	return render_template('list.html.jinja', quotes=quotes)
+    return render_template('list.html.jinja', quotes=quotes)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if session.get('logged_in'):
-		flash('You are already logged in!')
-		return redirect(url_for('home'))
+    if session.get('logged_in'):
+        flash('You are already logged in!')
+        return redirect(url_for('home'))
 
-	if request.method == 'GET' or request.method == 'HEAD':
-		return render_template('login.html.jinja', form_action=url_for('login'))
+    if request.method == 'GET' or request.method == 'HEAD':
+        return render_template(
+            'login.html.jinja',
+            form_action=url_for('login')
+        )
 
-	if request.form.get('password') == app.config.get('PASSWORD'):
-		session['logged_in'] = True
-		flash('Successfully logged in!')
-		return redirect(url_for('home'))
+    if request.form.get('password') == app.config.get('PASSWORD'):
+        session['logged_in'] = True
+        flash('Successfully logged in!')
+        return redirect(url_for('home'))
 
-	flash('Incorrect password!')
-	return redirect(url_for('login'))
+    flash('Incorrect password!')
+    return redirect(url_for('login'))
 
 
 @app.route('/<int:quote_id>', methods=['GET', 'DELETE', 'PATCH'])
 def show(quote_id):
-	quote = db_session.query(Quote) \
-		.filter(Quote.id == quote_id) \
-		.first()
+    quote = db_session.query(Quote) \
+        .filter(Quote.id == quote_id) \
+        .first()
 
-	if not quote:
-		return abort(404)
+    if not quote:
+        return abort(404)
 
-	if request.method == 'GET' or request.method == 'HEAD':
-		if request.headers.get('Accept') == 'application/json':
-			return jsonify(quote=quote)
-		return render_template('show.html.jinja', quote=quote)
+    if request.method == 'GET' or request.method == 'HEAD':
+        if request.headers.get('Accept') == 'application/json':
+            return jsonify(quote=quote)
+        return render_template('show.html.jinja', quote=quote)
 
-	if not session.get('logged_in'):
-		return abort(401)
+    if not session.get('logged_in'):
+        return abort(401)
 
-	if request.method == 'DELETE':
-		db_session.delete(quote)
-		db_session.commit()
-		return 'OK'
+    if request.method == 'DELETE':
+        db_session.delete(quote)
+        db_session.commit()
+        return 'OK'
 
-	body = Quote.prepare(request.form['body'])
-	quote.body = body
-	db_session.add(quote)
-	db_session.commit()
-	return 'OK'
+    body = Quote.prepare(request.form['body'])
+    quote.body = body
+    db_session.add(quote)
+    db_session.commit()
+    return 'OK'
 
 
 @app.route('/<int:quote_id>/approve', methods=['POST'])
 def approve(quote_id):
-	if not session.get('logged_in'):
-		return abort(401)
+    if not session.get('logged_in'):
+        return abort(401)
 
-	quote = db_session.query(Quote) \
-		.filter(Quote.id == quote_id) \
-		.first()
+    quote = db_session.query(Quote) \
+        .filter(Quote.id == quote_id) \
+        .first()
 
-	if not quote:
-		return abort(404)
+    if not quote:
+        return abort(404)
 
-	quote.approved = True
-	db_session.add(quote)
-	db_session.commit()
+    quote.approved = True
+    db_session.add(quote)
+    db_session.commit()
 
-	if app.config.get('ENABLE_IRCBOT_WEBHOOKS'):
-		ircbot.notify_bot(quote)
+    if app.config.get('ENABLE_IRCBOT_WEBHOOKS'):
+        ircbot.notify_bot(quote)
 
-	return 'OK'
+    return 'OK'
