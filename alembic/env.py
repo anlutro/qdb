26fb0f33b477c798996f93c0b5872011ea49f939
Andreas Lutro <anlutro@gmail.com>
2021-05-09 11:25:10 +0200

    replace travis-ci with github actions (#17)

--- alembic/env.py
+++ alembic/env.py
@@ -25,15 +25,15 @@ target_metadata = models.Base.metadata
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
-	This configures the context with just a URL
-	and not an Engine, though an Engine is acceptable
-	here as well.  By skipping the Engine creation
-	we don't even need a DBAPI to be available.
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
 
-	Calls to context.execute() here emit the given string to the
-	script output.
+    Calls to context.execute() here emit the given string to the
+    script output.
 
-	"""
+    """
     url = app.config.get("DB")
     context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
 
@@ -44,10 +44,10 @@ def run_migrations_offline():
 def run_migrations_online():
     """Run migrations in 'online' mode.
 
-	In this scenario we need to create an Engine
-	and associate a connection with the context.
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
 
-	"""
+    """
     connectable = database.engine
 
     with connectable.connect() as connection:
